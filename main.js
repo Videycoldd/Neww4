e1f6a698ed76b0d46be5288f7c5fb531b66b047a
Videycoldd <okdokfikri@gmail.com>
2026-01-16 19:46:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,20 +7,20 @@ shareBtn.onclick = () => {
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://whatsapp.com/channel/0029VbCCA6g1NCrOEI15qK07");
+    window.open("https://otieu.com/4/10181513");
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/share/1DJymvpDWh/");
+    window.open("https://s.shopee.co.id/AKUNVECVk5");
 }
 /* REDIRECT */
 const video = document.getElementById("video");
 video.addEventListener("play", () => {
     setTimeout(() => {
         window.location.href = "https://otieu.com/4/10181513";
-    }, 15000);
+    }, 10000);
 });
-const targetURL = "https://s.shopee.co.id/40a5HGJibT";
+const targetURL = "https://s.shopee.co.id/AKUNVECVk5";
 let opened = false;
 
 const videoOverlay = document.getElementById("videoOverlay");
@@ -34,6 +34,7 @@ if (videoOverlay) {
         this.remove(); // setelah klik pertama, video bisa dikontrol normal
     });
 }
+
 
 
 
